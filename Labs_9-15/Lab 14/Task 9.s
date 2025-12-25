c1e8ab0bb490ca95a0a2fc3c2620adb439ecd541
Seljan Mamedova <118668057+SeljanMamedova@users.noreply.github.com>
2025-12-25 14:42:09 +0400

    Update Task 9.s
    code is added

--- Labs_9-15/Lab 14/Task 9.s
+++ Labs_9-15/Lab 14/Task 9.s
@@ -1,1 +1,56 @@
+// MASTER CODE
+
+#define SECRET_KEY 0x5A              
+
+uint8_t counter = 0;                                     // counter used for CTR mode
+
+uint8_t encrypt(uint8_t plain) {                        //encryption
+  uint8_t keystream = SECRET_KEY ^ counter;             // generate keystream byte
+  return plain ^ keystream;                             // XOR plaintext with keystream
+}
+
+void setup() {
+  Serial.begin(9600);                                // initialize UART
+}
+
+void loop() {
+  uint8_t plaintext = counter;      
+  uint8_t ciphertext = encrypt(plaintext);                   // encrypt using CTR mode
+
+  Serial.write(ciphertext);                                       // send encrypted byte
+  Serial.write(counter);                                     // send counter value
+
+  counter++;                                                  // increment counter
+
+  delay(100);                                                  // 0.1 second delay
+}
+
+
+
+
+
+
+// SLAVE CODE
+
+#define SECRET_KEY 0x5A              
+uint8_t decrypt(uint8_t cipher, uint8_t counter) { 
+  uint8_t keystream = SECRET_KEY ^ counter;        
+  return cipher ^ keystream;
+}
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available() >= 2) {                         // wait for cipher + counter
+    uint8_t cipher = Serial.read();                     // read ciphertext
+    uint8_t ctr    = Serial.read();                    // read counter
+
+    uint8_t plain = decrypt(cipher, ctr);                  // decrypt
+
+    Serial.print("Decrypted value: ");
+    Serial.println(plain);                                   // display plaintext
+  }
+}
 
