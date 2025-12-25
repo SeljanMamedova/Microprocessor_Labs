677a06e9425e407c9859b7ccd9186e045aa74639
Seljan Mamedova <118668057+SeljanMamedova@users.noreply.github.com>
2025-12-25 12:26:25 +0400

    Update Task4.s
    Changes on comments' part

--- Labs_9-15/Lab 11/Task4.s
+++ Labs_9-15/Lab 11/Task4.s
@@ -1,128 +1,76 @@
 //Code For Master
 
-// SPI MASTER – ATmega328P (Arduino Uno)
+#include <avr/io.h>              
+#include <util/delay.h>                                // delay functions
 
-#include <avr/io.h>
-#include <util/delay.h>
-
-// ----------------------------------------------------
-// Initialize SPI in Master mode
-// ----------------------------------------------------
-void SPI_MasterInit(void)
+void SPI_MasterInit(void)                             // initialize SPI in master mode
 {
-    // Set MOSI (PB3), SCK (PB5), SS (PB2) as OUTPUT
-    DDRB |= (1 << DDB3) | (1 << DDB5) | (1 << DDB2);
+    DDRB |= (1 << DDB3) | (1 << DDB5) | (1 << DDB2);     // MOSI, SCK, SS as outputs
 
-    // Set SS HIGH (slave not selected)
-    PORTB |= (1 << PORTB2);
+    PORTB |= (1 << PORTB2);                             // set SS high (slave not selected)
 
-    // Enable SPI
-    // MSTR = 1 → Master mode
-    // SPR0 = 1 → Clock = fosc / 16
-    SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR0);
+    SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR0);      // enable SPI, master mode
 }
 
-// ----------------------------------------------------
-// Transmit one byte via SPI
-// ----------------------------------------------------
-void SPI_MasterTransmit(uint8_t data)
+void SPI_MasterTransmit(uint8_t data) 
 {
-    // Pull SS LOW → select slave
-    PORTB &= ~(1 << PORTB2);
+    PORTB &= ~(1 << PORTB2);                            // pull SS low to select slave
 
-    // Load data into SPI data register
-    // Transmission starts automatically
-    SPDR = data;
+    SPDR = data;                                       // load data into SPI data register
 
-    // Wait until transmission is complete
-    // SPIF flag becomes 1 when done
-    while (!(SPSR & (1 << SPIF)));
+    while (!(SPSR & (1 << SPIF)));                    // wait until transmission completes
 
-    // Pull SS HIGH → release slave
-    PORTB |= (1 << PORTB2);
+    PORTB |= (1 << PORTB2);                          // pull SS high to release slave
 }
 
-// ----------------------------------------------------
-// Main program
-// ----------------------------------------------------
 int main(void)
 {
-    // Initialize SPI as master
     SPI_MasterInit();
 
-    // Array of values to send to slave
-    uint8_t values[3] = {85, 170, 255};
-    uint8_t i = 0;
+    uint8_t values[3] = {85, 170, 255};                     // values to send to slave
+    uint8_t i = 0;                                         // index for values array
 
     while (1)
     {
-        // Send current value via SPI
-        SPI_MasterTransmit(values[i]);
+        SPI_MasterTransmit(values[i]);                    // send current value
 
-        // Move to next value (0 → 1 → 2 → 0)
-        i = (i + 1) % 3;
+        i = (i + 1) % 3;                                 // move to next value
 
-        // Wait 1 second between transmissions
-        _delay_ms(1000);
+        _delay_ms(1000);                                // delay 1 second between transmissions
     }
 }
 
 
 //Code For Slave 
 
-// SPI SLAVE – ATmega328P (Arduino Uno)
-
 #include <avr/io.h>
-#include <Arduino.h>
+#include <Arduino.h>             
 
-// ----------------------------------------------------
-// Initialize SPI in Slave mode
-// ----------------------------------------------------
-void SPI_SlaveInit(void)
+void SPI_SlaveInit(void)                           // initialize SPI in slave mode
 {
-    // Set MISO (PB4) as OUTPUT
-    // Slave sends data to master through MISO
-    DDRB |= (1 << DDB4);
+    DDRB |= (1 << DDB4);                         // set MISO as output
 
-    // Enable SPI (Slave mode by default)
     SPCR = (1 << SPE);
 }
 
-// ----------------------------------------------------
-// Receive one byte via SPI
-// ----------------------------------------------------
-uint8_t SPI_SlaveReceive(void)
+uint8_t SPI_SlaveReceive(void)  
 {
-    // Wait until SPI reception is complete
-    // SPIF flag is set when data is received
-    while (!(SPSR & (1 << SPIF)));
+    while (!(SPSR & (1 << SPIF)));                  // wait until data is received
 
-    // Read received data from SPI Data Register
-    return SPDR;
+    return SPDR;                                  // return received data
 }
 
-// ----------------------------------------------------
-// Arduino setup function
-// ----------------------------------------------------
 void setup()
 {
-    // Initialize serial communication for monitoring
-    Serial.begin(9600);
+    Serial.begin(9600);                       // initialize serial communication
 
-    // Initialize SPI as slave
-    SPI_SlaveInit();
+    SPI_SlaveInit();                         // initialize SPI slave
 }
 
-// ----------------------------------------------------
-// Main loop
-// ----------------------------------------------------
 void loop()
 {
-    // Receive data sent by SPI master
-    uint8_t received = SPI_SlaveReceive();
+    uint8_t received = SPI_SlaveReceive();               // receive data from master
 
-    // Print received data to Serial Monitor
-    Serial.print("Received: ");
-    Serial.println(received);
+    Serial.print("Received: ");                         // print label
+    Serial.println(received);                          // print received value
 }
-
